0c20a080b5369fc12d0da4a9e3e5f3f88189a8f9
Przemysław Lange <przemek.lange@gmail.com>
2022-09-20 15:18:18 +0200

    add header and sidebar and clear project

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FeaturedInfo from "../../Components/featuredInfo/FeaturedInfo";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <FeaturedInfo />
+    </div>
+  );
+};
+
+export default Home;
